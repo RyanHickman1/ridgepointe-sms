6f5df9102db505d7af819caf0628164471103f41
RyanHickman1 <cvaryan22701@gmail.com>
2026-01-29 18:06:57 -0600

    Delete api directory

--- api/send-owner-sms.js
+++
@@ -1,6 +0,0 @@
-module.exports = async (req, res) => {
-  if (req.method !== "POST") {
-    return res.status(200).send("OK (use POST for JSON)");
-  }
-  return res.status(200).json({ ok: true, received: req.body });
-};
